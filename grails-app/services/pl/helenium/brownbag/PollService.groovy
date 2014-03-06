28b121c9f095aee69a3474f765533c2b1170b063
Michal Mally <michal@mally.pl>
2014-03-06 23:12:47 +0100

    Creating new idea is now done by low-level API.

--- grails-app/services/pl/helenium/brownbag/PollService.groovy
+++ grails-app/services/pl/helenium/brownbag/PollService.groovy
@@ -7,20 +7,30 @@ class PollService {
     }
 
     void createIdea(String pollId, Idea idea) {
-        def poll = Poll.findById(pollId)
-        idea.id = UUID.randomUUID().toString()
-        poll.addToIdeas(idea)
-        poll.save()
+        Poll.collection.update(
+                [_id: pollId],
+                [
+                        $addToSet: [
+                                ideas: [
+                                        _id: UUID.randomUUID().toString(),
+                                        title: idea.title,
+                                        location: idea.location,
+                                        duration: idea.duration,
+                                        creator: idea.creator,
+                                        votes: idea.votes,
+                                ]
+                        ]
+                ])
     }
 
     void voteIdea(String pollId, String ideaId, User user) {
         def poll = Poll.findById(pollId)
         Idea idea = poll.ideas.find { it.id == ideaId }
-        assert idea : "idea mustn't be null"
+        assert idea: "idea mustn't be null"
 
         def userId = new UserId(name: user.name)
         userId.id = user.providerId
-        if (idea.votes.every() { it.id != userId.id}) {
+        if (idea.votes.every() { it.id != userId.id }) {
             idea.addToVotes(userId)
             poll.$changedProperties['ideas'] = true
             poll.save(flush: true, failOnError: true)

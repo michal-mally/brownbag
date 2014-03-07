0c7bbeefdbd796260dda37d127990a8da11530b7
Michal Mally <michal@mally.pl>
2014-03-07 23:34:03 +0100

    Preparation for dynamic linking inside docker container.

--- webmodule/grails-app/conf/DataSource.groovy
+++ webmodule/grails-app/conf/DataSource.groovy
@@ -1,6 +1,6 @@
 grails {
     mongo {
-        host = System.getenv("MONGO_HOST")
+        host = System.getenv("MONGO_PORT_27017_TCP_ADDR")
         port = 27017
         databaseName = "brownbag"
     }

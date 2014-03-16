66cd890077cf24f5919f46f9668d7177c15e6e22
Michal Mally <michal@mally.pl>
2014-03-16 14:34:31 +0100

    Minor cleanup.

--- webmodule/grails-app/views/layouts/brownbag.gsp
+++ webmodule/grails-app/views/layouts/brownbag.gsp
@@ -5,9 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <link rel="stylesheet" href="${resource(dir: 'css', file: 'brownbag.css')}" type="text/css">
-    <script type="text/javascript">
-        var _contextPath = "${request.contextPath}";
-    </script>
     <r:require modules="bootstrap"/>
     <r:require modules="brownbag"/>
     <g:layoutHead/>
--- webmodule/web-app/js/angular/services.js
+++ webmodule/web-app/js/angular/services.js
@@ -1,10 +1,10 @@
 brownbag.service('authService', function ($http) {
     var getLoggedInUser = function () {
-        return $http.get(_contextPath + '/auth/user.json');
+        return $http.get('auth/user.json');
     };
 
     var logout = function() {
-        return $http.post(_contextPath + '/auth/logout');
+        return $http.post('auth/logout');
     };
 
     return {
@@ -16,19 +16,19 @@ brownbag.service('authService', function ($http) {
 brownbag.service('pollService', function ($http) {
     return {
         createPoll: function(poll) {
-            return $http.post(_contextPath + '/poll/create.json', poll);
+            return $http.post('poll/create.json', poll);
         },
         getPoll: function (pollId) {
-            return $http.get(_contextPath + '/poll/show/' + pollId + '.json');
+            return $http.get('poll/show/' + pollId + '.json');
         },
         createIdea: function(pollId, idea) {
-            return $http.post(_contextPath + '/poll/createIdea/' + pollId, idea);
+            return $http.post('poll/createIdea/' + pollId, idea);
         },
         voteIdea: function (pollId, ideaId) {
-            return $http.post(_contextPath + '/poll/voteIdea/' + pollId, {ideaId: ideaId});
+            return $http.post('poll/voteIdea/' + pollId, {ideaId: ideaId});
         },
         getUserActivity: function() {
-            return $http.get(_contextPath + '/poll/showUserActivity.json');
+            return $http.get('poll/showUserActivity.json');
         }
     }
 });

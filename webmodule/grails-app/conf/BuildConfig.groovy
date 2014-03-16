696d44f908531b9bcafadfbc9949b7691c623b70
Michal Mally <michal@mally.pl>
2014-03-16 08:53:11 +0100

    Got rid of scaffolding related configuration.

--- webmodule/grails-app/conf/BuildConfig.groovy
+++ webmodule/grails-app/conf/BuildConfig.groovy
@@ -59,7 +59,6 @@ grails.project.dependency.resolution = {
         build ":tomcat:7.0.52.1"
 
         // plugins for the compile step
-        // compile ":scaffolding:2.0.2"
         compile ':cache:1.1.1'
 
         // plugins needed at runtime but not for compilation
